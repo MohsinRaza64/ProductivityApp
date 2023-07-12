9dff25eb83c6613f3f570fcad94bff1ae202e1ed
mraza <mraza.0903@gmail.com>
2023-07-12 19:52:26 +0500

    Home Screen without list. Only non-functional top and bottom icons.

---
+++ app/src/main/java/com/example/productivityapp/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.productivityapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.productivityapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
