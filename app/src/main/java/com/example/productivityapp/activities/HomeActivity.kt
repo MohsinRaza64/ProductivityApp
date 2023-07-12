341bd23d610ec6a4f30951667d21490275ae621f
mraza <mraza.0903@gmail.com>
2023-07-12 20:12:56 +0500

    Home Screen without list. Only non-functional top and bottom icons. With dummy on click that restarts the app.

--- app/src/main/java/com/example/productivityapp/activities/HomeActivity.kt
+++ app/src/main/java/com/example/productivityapp/activities/HomeActivity.kt
@@ -1,12 +1,26 @@
 package com.example.productivityapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.productivityapp.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+    fun reStart(view: View){
+        startActivity(Intent(this,SplashScreen::class.java))
+    }
+    fun onHomeButtonClick(view: View){
+        startActivity(Intent(this,SplashScreen::class.java))
+    }
+    fun onAddButtonClick(view: View){
+        startActivity(Intent(this,SplashScreen::class.java))
+    }
+    fun onSettingButtonClick(view: View){
+        startActivity(Intent(this,SplashScreen::class.java))
     }
 }
