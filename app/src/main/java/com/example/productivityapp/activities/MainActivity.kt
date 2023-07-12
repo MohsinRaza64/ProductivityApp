49ef89e910b144e39e26841c681f986502b23eb2
mraza <mraza.0903@gmail.com>
2023-07-12 19:02:44 +0500

    Added theme

--- app/src/main/java/com/example/productivityapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/productivityapp/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.productivityapp
+package com.example.productivityapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.productivityapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/productivityapp/activities/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.example.productivityapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.productivityapp.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
