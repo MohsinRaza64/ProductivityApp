6bdff61f187a397f9b3403d5f6cf0febd9cafc50
mraza <mraza.0903@gmail.com>
2023-07-13 23:48:36 +0500

    Made the status icon clickable.

--- app/src/main/java/com/example/productivityapp/classes/TaskAdapter.kt
+++ app/src/main/java/com/example/productivityapp/classes/TaskAdapter.kt
@@ -3,7 +3,10 @@ package com.example.productivityapp.classes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.productivityapp.R
 
@@ -20,10 +23,35 @@ class TaskAdapter(
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val task = tasks[position]
+
+        var constraintLayout = holder.itemView.findViewById<ConstraintLayout>(R.id.cardViewConstraintlayout)
+
         val titleTextView = holder.itemView.findViewById<TextView>(R.id.titleTextView)
         val startTimeTextView = holder.itemView.findViewById<TextView>(R.id.timeTextView)
+        val statusImageView = holder.itemView.findViewById<ImageView>(R.id.imageViewStatus)
         titleTextView.text = task.title
         startTimeTextView.text = task.startTime.toString()
+
+
+        //Handling Tick/Cross Icon Click.
+        statusImageView.setOnClickListener{
+            if(task.status == false){
+                task.status = true
+                statusImageView.setImageResource(R.drawable.tick)
+            } else {
+                task.status = false
+                statusImageView.setImageResource(R.drawable.cross)
+            }
+        }
+
+        //setting appropriate status icon
+        if(task.status == false) {
+            statusImageView.setImageResource(R.drawable.cross)
+//            constraintLayout.setBackgroundResource(R.drawable.pink_gradient);
+//            startTimeTextView.setTextColor(ContextCompat.getColor(startTimeTextView.context, R.color.white))
+        }
+
+
     }
 
     override fun getItemCount(): Int {
