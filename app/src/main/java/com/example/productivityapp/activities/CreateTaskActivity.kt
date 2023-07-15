ed412bdee15e3c5ba72ff6f817dbc4a3d1aae8e7
mraza <mraza.0903@gmail.com>
2023-07-15 16:45:53 +0500

    CreateTaskActivity's UI Done in XML.

--- app/src/main/java/com/example/productivityapp/activities/CreateTaskActivity.kt
+++ app/src/main/java/com/example/productivityapp/activities/CreateTaskActivity.kt
@@ -7,16 +7,26 @@ import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import com.example.productivityapp.R
+import java.time.LocalTime
 import java.util.Calendar
 
 class CreateTaskActivity : AppCompatActivity() {
     private lateinit var stEditText : EditText
+    private lateinit var etEditText : EditText
 
+    //Data variables
+    lateinit var startTimeS : LocalTime
+    lateinit var endTimeS : LocalTime
+    var iconIdS : Int = 0
+    var iconIdWS : Int = 0
+    var statusS : Boolean = false
+    //////////////////////////////////////////
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_task)
 
         stEditText = findViewById(R.id.startTimeEditText)
+        etEditText = findViewById(R.id.endTimeEditText)
 
     }
 
@@ -24,8 +34,17 @@ class CreateTaskActivity : AppCompatActivity() {
         val currentTime = Calendar.getInstance()
         val initHour = currentTime.get(Calendar.HOUR_OF_DAY)
         val initMinute = currentTime.get(Calendar.MINUTE)
-        TimePickerDialog(this, { view, hour_of_day, minute ->
+        TimePickerDialog(this, R.style.TimePickerDialog, { view, hour_of_day, minute ->
             stEditText.setText("$hour_of_day:$minute")
+        }, initHour, initMinute, true).show()
+    }
+
+    fun setEndTime(view: View) {
+        val currentTime = Calendar.getInstance()
+        val initHour = currentTime.get(Calendar.HOUR_OF_DAY)
+        val initMinute = currentTime.get(Calendar.MINUTE)
+        TimePickerDialog(this, R.style.TimePickerDialog, { view, hour_of_day, minute ->
+            etEditText.setText("$hour_of_day:$minute")
         }, initHour, initMinute, true).show()
     }
 }
